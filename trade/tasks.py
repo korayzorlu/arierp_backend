3cc34154c92988727b613bd83fd3e585de05f642
korayzorlu <korayzorllu@gmail.com>
2025-07-02 13:46:32 +0300

    #20250702 fix: fixed global settings

---
+++ trade/tasks.py
@@ -0,0 +1,69 @@
+from celery import shared_task
+from core.celery import app
+from django.http import JsonResponse
+
+import pandas as pd
+import io
+import pyodbc
+
+from .models import *
+from users.models import User
+from contracts.models import *
+from common.models import Currency
+
+@shared_task()
+def transfer_trade_accounts(company):
+    SERVER = "192.168.81.8,1433"
+    DATABASE = "ARI_LEASING"
+    USERNAME = "lflex"
+    PASSWORD = "S!gma2014"
+
+    connectionString = f'''
+        DRIVER={{ODBC Driver 18 for SQL Server}};
+        SERVER={SERVER};
+        DATABASE={DATABASE};
+        UID={USERNAME};
+        PWD={PASSWORD};
+        Provider=SQLNCLI11;
+        Integrated Security=SSPI;
+        Persist Security Info=False;
+        Initial Catalog=MASTER;
+        TrustServerCertificate=yes;
+    '''
+
+    try:
+        conn = pyodbc.connect(connectionString)
+        
+        SQL_QUERY = """
+        SELECT *
+        FROM TradeAccount
+        ORDER BY AccId
+        """
+
+        cursor = conn.cursor()
+        cursor.execute(SQL_QUERY)
+        
+        records = cursor.fetchall()
+        external_data=[
+            {   
+                "partner" : r.AccCrmId,
+                "crm_type" : r.AccCrmType,
+                "account_id" : r.AccId,
+                "name" : r.AccName,
+            }
+            for r in records
+        ]
+        print(len(external_data))
+        for data in external_data:
+            if TradeAccount.objects.select_related().filter(account_id = str(int(data["account_id"]))).exists():
+                continue
+            obj = TradeAccount.objects.create(
+                company = Company.objects.select_related().filter(id = int(company)).first(),
+                partner = Partner.objects.select_related().filter(crm_code = str(data["partner"])).first() if not pd.isna(data["partner"]) else None,
+                account_id = str(int(data["account_id"])) if not pd.isna(data["account_id"]) else "",
+                crm_type = str(data["crm_type"]) if not pd.isna(data["crm_type"]) else "",
+                name = data["name"] if not pd.isna(data["name"]) else "",
+            )
+            obj.save()
+    except Exception as e:
+        print(e)
