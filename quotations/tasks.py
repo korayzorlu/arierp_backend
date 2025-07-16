f874bb31fe2e0b763c57b1e575d155f6a48078e5
korayzorlu <korayzorllu@gmail.com>
2025-07-16 10:56:48 +0300

    #20250716 fix: fixed global settings

--- quotations/tasks.py
+++ quotations/tasks.py
@@ -187,3 +187,112 @@ def fix_quick_quotation(company):
 
     except Exception as e:
         print(e)
+
+@shared_task()
+def fix_quotation(company):
+    SERVER = "192.168.81.8,1433"
+    DATABASE = "ARI_LEASING"
+    USERNAME = "lflex"
+    PASSWORD = "S!gma2014"
+
+    connectionString = f'''
+        DRIVER={{ODBC Driver 18 for SQL Server}};
+        SERVER={SERVER};
+        DATABASE={DATABASE};
+        UID={USERNAME};
+        PWD={PASSWORD};
+        Provider=SQLNCLI11;
+        Integrated Security=SSPI;
+        Persist Security Info=False;
+        Initial Catalog=MASTER;
+        TrustServerCertificate=yes;
+    '''
+
+    try:
+        conn = pyodbc.connect(connectionString)
+        
+        SQL_QUERY = """
+        SELECT QuotationHeaderId,
+            SubStatuteDefinition,
+            CustomerId,
+            CurrencyCode,
+            LeasingBaseCost,
+            CustomerRepresentative,
+            RequestDate,
+            Vendor,
+            Project
+        FROM QuotationHeaderLightList
+        """
+
+        cursor = conn.cursor()
+        cursor.execute(SQL_QUERY)
+        
+        records = cursor.fetchall()
+
+        external_data=[
+            {
+                "QuotationHeaderId" : r.QuotationHeaderId,
+                "SubStatuteDefinition" : r.SubStatuteDefinition,
+                "CustomerId" : r.CustomerId,
+                "CurrencyCode" : r.CurrencyCode,
+                "LeasingBaseCost" : r.LeasingBaseCost,
+                "CustomerRepresentative" : r.CustomerRepresentative,
+                "RequestDate" : r.RequestDate,
+                "Vendor" : r.Vendor,
+                "Project" : r.Project,
+            }
+            for r in records
+        ]
+
+        quotations = Quotation.objects.select_related("status","currency","company").all()
+        statuses = Status.objects.select_related().all()
+        partners = Partner.objects.select_related().all()
+        currencies = Currency.objects.select_related().all()
+        company_obj = Company.objects.select_related().filter(id=int(company)).first()
+
+        quotation_by_code = {q.code: q for q in quotations if q.code}
+        statuses_dict = {s.name: s for s in statuses}
+        partners_dict = {p.crm_code: p for p in partners}
+        currencies_dict = {c.code: c for c in currencies}
+
+        previous_progress = 0
+        for index,data in enumerate(external_data):
+            current_progress = ((index + 1)/len(external_data))*100
+
+            if current_progress - previous_progress >= 1:
+                previous_progress = current_progress
+                print(f"{int(current_progress)} %")
+
+            if str(data["QuotationHeaderId"]):
+                obj = (quotation_by_code.get(str(data["QuotationHeaderId"])))
+            else:
+                obj = None
+
+            if obj:
+                obj.code = str(data["QuotationHeaderId"]) or ""
+                obj.status = statuses_dict.get(normalize(data["SubStatuteDefinition"]))
+                obj.partner = partners_dict.get(str(data["CustomerId"]))
+                obj.currency = currencies_dict.get("TRY" if data["CurrencyCode"] == "TL" else data["CurrencyCode"])
+                obj.kbm = safe_decimal(data["LeasingBaseCost"])
+                obj.customer_representative = data["CustomerRepresentative"] or ""
+                obj.request_date = data["RequestDate"].date() if data["RequestDate"] else None
+                obj.supplier = data["Vendor"] or ""
+                obj.project = data["Project"] or ""
+                obj.save()
+            else:
+                print(f"{str(data["QuotationHeaderId"])} - {data["CustomerId"]}: ")
+                Quotation.objects.create(
+                    company = company_obj,
+                    code = str(data["QuotationHeaderId"]) or "",
+                    status = statuses_dict.get(normalize(data["SubStatuteDefinition"])),
+                    partner = partners_dict.get(str(data["CustomerId"])),
+                    currency = currencies_dict.get("TRY" if data["CurrencyCode"] == "TL" else data["CurrencyCode"]),
+                    kbm = safe_decimal(data["LeasingBaseCost"]),
+                    customer_representative = data["CustomerRepresentative"] or "",
+                    request_date = data["RequestDate"].date() if data["RequestDate"] else None,
+                    supplier = data["Vendor"] or "",
+                    project = data["Project"] or ""
+                )
+
+    except Exception as e:
+        print(e)
