74e3eedf659330f3c39535baeeaff6f4d56de772
korayzorlu <korayzorllu@gmail.com>
2025-07-22 11:02:57 +0300

    #20250722 fix: fixed global settings

--- leasing/tests.py
+++ leasing/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 # Create your tests here.from decimal import Decimal, ROUND_HALF_UP
 from .models import Installment
 
-installments = Installment.objects.select_related("lease").all()
-installment_by_code = {(i.lease.lease_id, i.sequency): i for i in installments if i.lease.lease_id and i.sequency}
-obj = (installment_by_code.get(("91556",0)))
+# installments = Installment.objects.select_related("lease").all()
+# installment_by_code = {(i.lease.lease_id, i.sequency): i for i in installments if i.lease.lease_id and i.sequency}
+# obj = (installment_by_code.get(("91556",0)))
 
-print(obj)
+# print(obj)
