c294d5fd82157ac7b01e93024fb7a08cb4314181
korayzorlu <korayzorllu@gmail.com>
2025-08-21 07:10:46 +0300

    #20250821 fix: fixed global settings

--- purchasing/api/filters.py
+++ purchasing/api/filters.py
@@ -19,7 +19,7 @@ class PurchasePaymentFilter(FilterSet):
     partner = CharFilter(field_name='lease__contract__partner__name', lookup_expr='icontains')
     vendor = CharFilter(field_name='lease__contract__vendor__name', lookup_expr='icontains')
     currency = CharFilter(field_name='lease__currency__code', lookup_expr='icontains')
-    project = CharFilter(field_name='lease__contract__project__name', lookup_expr='icontains')
+    project = CharFilter(field_name='lease__contract__project', lookup_expr='icontains')
     status = CharFilter(field_name='lease__status__name', lookup_expr='icontains')
     lease_status = CharFilter(field_name='lease__lease_status', lookup_expr='icontains')  
 
