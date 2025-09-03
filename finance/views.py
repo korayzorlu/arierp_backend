f8b5773032428937797947a7e2305b1df9bb6ca5
korayzorlu <korayzorllu@gmail.com>
2025-09-03 08:24:28 +0300

    #20250903 fix: fixed global settings

--- finance/views.py
+++ finance/views.py
@@ -40,31 +40,88 @@ class FinanceSummaryView(LoginRequiredMixin,View):
         active_company_uuid = data.get('params').get('activeCompany').get('id')
         active_company = request.user.user_companies.filter(uuid = active_company_uuid).first()
 
-        vendors = PurchasePayment.objects.select_related("lease__contract__vendor").prefetch_related().filter(
+        vendors_try = PurchasePayment.objects.select_related("lease__contract__vendor").prefetch_related().filter(
             Q(company = active_company.company if active_company else None) &
-            vendor_filter_for_serializers(data.get('params')) 
+            vendor_filter_for_serializers(data.get('params'))  &
+            Q(lease__currency__code = "TRY")
         ).aggregate(
+            total_total_contract_amount=Sum('total_contract_amount'),
             total_before_total_payment=Sum('before_total_payment'),
             total_after_total_payment=Sum('after_total_payment'),
-            total_managing_expense=Sum('managing_expense')
+            total_managing_expense=Sum('managing_expense'),
+            total_lease_payment_amount=Sum('lease_payment_amount'),
+            total_total_vendor_payment=Sum('total_vendor_payment')
+        )
+
+        vendors_usd = PurchasePayment.objects.select_related("lease__contract__vendor").prefetch_related().filter(
+            Q(company = active_company.company if active_company else None) &
+            vendor_filter_for_serializers(data.get('params'))  &
+            Q(lease__currency__code = "USD")
+        ).aggregate(
+            total_total_contract_amount=Sum('total_contract_amount'),
+            total_before_total_payment=Sum('before_total_payment'),
+            total_after_total_payment=Sum('after_total_payment'),
+            total_managing_expense=Sum('managing_expense'),
+            total_lease_payment_amount=Sum('lease_payment_amount'),
+            total_total_vendor_payment=Sum('total_vendor_payment')
+        )
+
+        vendors_eur = PurchasePayment.objects.select_related("lease__contract__vendor").prefetch_related().filter(
+            Q(company = active_company.company if active_company else None) &
+            vendor_filter_for_serializers(data.get('params'))  &
+            Q(lease__currency__code = "EUR")
+        ).aggregate(
+            total_total_contract_amount=Sum('total_contract_amount'),
+            total_before_total_payment=Sum('before_total_payment'),
+            total_after_total_payment=Sum('after_total_payment'),
+            total_managing_expense=Sum('managing_expense'),
+            total_lease_payment_amount=Sum('lease_payment_amount'),
+            total_total_vendor_payment=Sum('total_vendor_payment')
         )
 
 
         manager_summary = [
             {   
                 'id': 1,
-                'title': 'Ödeme Toplam Öncesi',
-                'amount': float(vendors['total_before_total_payment']) if vendors['total_before_total_payment'] else 0.00
+                'title': 'Toplam Sözleşme Bedeli',
+                'amount_try': float(vendors_try['total_total_contract_amount']) if vendors_try['total_total_contract_amount'] else 0.00,
+                'amount_usd': float(vendors_usd['total_total_contract_amount']) if vendors_usd['total_total_contract_amount'] else 0.00,
+                'amount_eur': float(vendors_eur['total_total_contract_amount']) if vendors_eur['total_total_contract_amount'] else 0.00
             },
             {   
                 'id': 2,
-                'title': 'Toplam Ödeme Sonrası',
-                'amount': float(vendors['total_after_total_payment']) if vendors['total_after_total_payment'] else 0.00
+                'title': 'Ödeme Toplam Öncesi',
+                'amount_try': float(vendors_try['total_before_total_payment']) if vendors_try['total_before_total_payment'] else 0.00,
+                'amount_usd': float(vendors_usd['total_before_total_payment']) if vendors_usd['total_before_total_payment'] else 0.00,
+                'amount_eur': float(vendors_eur['total_before_total_payment']) if vendors_eur['total_before_total_payment'] else 0.00
             },
             {   
                 'id': 3,
-                'title': 'Yönetim Gideri',
-                'amount': float(vendors['total_managing_expense']) if vendors['total_managing_expense'] else 0.00
+                'title': 'Toplam Ödeme Sonrası',
+                'amount_try': float(vendors_try['total_after_total_payment']) if vendors_try['total_after_total_payment'] else 0.00,
+                'amount_usd': float(vendors_usd['total_after_total_payment']) if vendors_usd['total_after_total_payment'] else 0.00,
+                'amount_eur': float(vendors_eur['total_after_total_payment']) if vendors_eur['total_after_total_payment'] else 0.00
+            },
+            {   
+                'id': 4,
+                'title': 'Toplam Yönetim Gideri',
+                'amount_try': float(vendors_try['total_managing_expense']) if vendors_try['total_managing_expense'] else 0.00,
+                'amount_usd': float(vendors_usd['total_managing_expense']) if vendors_usd['total_managing_expense'] else 0.00,
+                'amount_eur': float(vendors_eur['total_managing_expense']) if vendors_eur['total_managing_expense'] else 0.00
+            },
+            {   
+                'id': 5,
+                'title': 'Toplam Kira Tahsilat Tutarı',
+                'amount_try': float(vendors_try['total_lease_payment_amount']) if vendors_try['total_lease_payment_amount'] else 0.00,
+                'amount_usd': float(vendors_usd['total_lease_payment_amount']) if vendors_usd['total_lease_payment_amount'] else 0.00,
+                'amount_eur': float(vendors_eur['total_lease_payment_amount']) if vendors_eur['total_lease_payment_amount'] else 0.00
+            },
+            {   
+                'id': 6,
+                'title': 'Toplam Satıcı Ödemeleri Tutarı',
+                'amount_try': float(vendors_try['total_total_vendor_payment']) if vendors_try['total_total_vendor_payment'] else 0.00,
+                'amount_usd': float(vendors_usd['total_total_vendor_payment']) if vendors_usd['total_total_vendor_payment'] else 0.00,
+                'amount_eur': float(vendors_eur['total_total_vendor_payment']) if vendors_eur['total_total_vendor_payment'] else 0.00
             },
         ]
 
