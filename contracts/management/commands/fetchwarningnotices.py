f9a49db2773c33e5336624f53025e25a92a05ea4
korayzorlu <korayzorllu@gmail.com>
2025-09-03 10:09:01 +0300

    #20250903 fix: fixed global settings

--- contracts/management/commands/fetchwarningnotices.py
+++ contracts/management/commands/fetchwarningnotices.py
@@ -27,82 +27,82 @@ class Command(BaseCommand):
 
         print("processing...")
         
-        #fetch_warning_notices.delay(company)
+        fetch_warning_notices.delay(company)
 
-        SERVER = "192.168.82.31,1433"
-        DATABASE = "ARI_LEASING"
-        USERNAME = "lflex"
-        PASSWORD = "S!gma2014"
+        # SERVER = "192.168.82.31,1433"
+        # DATABASE = "ARI_LEASING"
+        # USERNAME = "lflex"
+        # PASSWORD = "S!gma2014"
 
-        connectionString = f'''
-            DRIVER={{ODBC Driver 18 for SQL Server}};
-            SERVER={SERVER};
-            DATABASE={DATABASE};
-            UID={USERNAME};
-            PWD={PASSWORD};
-            Provider=SQLNCLI11;
-            Integrated Security=SSPI;
-            Persist Security Info=False;
-            Initial Catalog=MASTER;
-            TrustServerCertificate=yes;
-        '''
+        # connectionString = f'''
+        #     DRIVER={{ODBC Driver 18 for SQL Server}};
+        #     SERVER={SERVER};
+        #     DATABASE={DATABASE};
+        #     UID={USERNAME};
+        #     PWD={PASSWORD};
+        #     Provider=SQLNCLI11;
+        #     Integrated Security=SSPI;
+        #     Persist Security Info=False;
+        #     Initial Catalog=MASTER;
+        #     TrustServerCertificate=yes;
+        # '''
 
-        try:
-            conn = pyodbc.connect(connectionString)
+        # try:
+        #     conn = pyodbc.connect(connectionString)
             
-            SQL_QUERY = """
-                SELECT RiskDocumentId,
-                    RiskHeaderId,
-                    CustomerId,
-                    ContractHeaderId,
-                    OrgContractHeaderId,
-                    Debit,
-                    ProcessStartDate,
-                    DailyWagesDate,
-                    ServiceDate,
-                    OfficialCancellationDate,
-                    Paid,
-                    Diff,
-                    State,
-                    ApprovalState,
-                    ResultId,
-                    PROCESS_SITUATION_ID
-                    FROM RiskDocumentWarningFollowListBaseLPDDOR
-                    WHERE
-                        (PROCESS_SITUATION_ID is null or ResultId in (0,1,2)) 
-                        AND 1=1
-                        --AND CustomerId=29308
-                        AND ResultId in (0,1)
-                        AND ContractHeaderId='53233/1'
-            """
+        #     SQL_QUERY = """
+        #         SELECT RiskDocumentId,
+        #             RiskHeaderId,
+        #             CustomerId,
+        #             ContractHeaderId,
+        #             OrgContractHeaderId,
+        #             Debit,
+        #             ProcessStartDate,
+        #             DailyWagesDate,
+        #             ServiceDate,
+        #             OfficialCancellationDate,
+        #             Paid,
+        #             Diff,
+        #             State,
+        #             ApprovalState,
+        #             ResultId,
+        #             PROCESS_SITUATION_ID
+        #             FROM RiskDocumentWarningFollowListBaseLPDDOR
+        #             WHERE
+        #                 (PROCESS_SITUATION_ID is null or ResultId in (0,1,2)) 
+        #                 AND 1=1
+        #                 --AND CustomerId=29308
+        #                 AND ResultId in (0,1)
+        #                 AND ContractHeaderId='53233/1'
+        #     """
 
-            cursor = conn.cursor()
-            cursor.execute(SQL_QUERY)
+        #     cursor = conn.cursor()
+        #     cursor.execute(SQL_QUERY)
             
-            records = cursor.fetchall()
+        #     records = cursor.fetchall()
 
-            external_data=[
-                {
-                    "RiskDocumentId" : r.RiskDocumentId,
-                    "RiskHeaderId" : r.RiskHeaderId,
-                    "CustomerId" : r.CustomerId,
-                    "OrgContractHeaderId" : r.OrgContractHeaderId,
-                    "Debit" : r.Debit,
-                    "ProcessStartDate" : r.ProcessStartDate,
-                    "DailyWagesDate" : r.DailyWagesDate,
-                    "ServiceDate" : r.ServiceDate,
-                    "OfficialCancellationDate" : r.OfficialCancellationDate,
-                    "Paid" : r.Paid,
-                    "Diff" : r.Diff,
-                    "State" : r.State,
-                    "ApprovalState" : r.ApprovalState,
-                }
-                for r in records
-            ]
+        #     external_data=[
+        #         {
+        #             "RiskDocumentId" : r.RiskDocumentId,
+        #             "RiskHeaderId" : r.RiskHeaderId,
+        #             "CustomerId" : r.CustomerId,
+        #             "OrgContractHeaderId" : r.OrgContractHeaderId,
+        #             "Debit" : r.Debit,
+        #             "ProcessStartDate" : r.ProcessStartDate,
+        #             "DailyWagesDate" : r.DailyWagesDate,
+        #             "ServiceDate" : r.ServiceDate,
+        #             "OfficialCancellationDate" : r.OfficialCancellationDate,
+        #             "Paid" : r.Paid,
+        #             "Diff" : r.Diff,
+        #             "State" : r.State,
+        #             "ApprovalState" : r.ApprovalState,
+        #         }
+        #         for r in records
+        #     ]
 
-            print(external_data)
+        #     print(external_data)
            
-        except Exception as e:
-            print(e)  
+        # except Exception as e:
+        #     print(e)  
         
         print("done!")
