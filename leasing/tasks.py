e8606068f83bd78904b35829461892154a5ef4b2
korayzorlu <korayzorllu@gmail.com>
2025-09-12 09:41:38 +0300

    #20250912 fix: fixed global settings

--- leasing/tasks.py
+++ leasing/tasks.py
@@ -1187,6 +1187,8 @@ def fetch_overdue_leases(company):
             old_obj_count += 1
             if not pd.isna(row['Oran']) and float(row['Oran'].replace("% ","")) >= 96.73 and float(row['Oran'].replace("% ","")) < 100:
                 obj.is_kdv_diff = True
+            else:
+                obj.is_kdv_diff = False
             obj.paid_rate = Decimal(str(row['Oran'].replace("% ",""))) if not pd.isna(row['Oran']) else Decimal("0.00")
             obj.total_payment = Decimal(str(row['Kdv Dahil Kira Toplamı'])) if not pd.isna(row['Kdv Dahil Kira Toplamı']) else Decimal("0.00")
             obj.paid = Decimal(str(row['Tahsilat Tutarı'])) if not pd.isna(row['Tahsilat Tutarı']) else Decimal("0.00")
