6db9973c76c06812205204f76d90c416f650a8bf
korayzorlu <korayzorllu@gmail.com>
2025-09-30 08:20:24 +0300

    #20250930 fix: fixed global settings

--- partners/management/commands/fetchtraderpartners.py
+++ partners/management/commands/fetchtraderpartners.py
@@ -41,19 +41,19 @@ class Command(BaseCommand):
         #     contract_count__gt=1
         # )
 
-        objs = Partner.objects.select_related().filter(customer_type="institutional")
+        #objs = Partner.objects.select_related().filter(customer_type="institutional")
 
         
 
-        # objs = Partner.objects.select_related().annotate(
-        #     unique_contract_count=Count(
-        #         CodePrefix('partner_contracts__code'),
-        #         distinct=True
-        #     )
-        # ).filter(
-        #     customer_type="individual",
-        #     unique_contract_count__gt=1
-        # )
+        objs = Partner.objects.select_related().annotate(
+            unique_contract_count=Count(
+                CodePrefix('partner_contracts__code'),
+                distinct=True
+            )
+        ).filter(
+            customer_type="individual",
+            unique_contract_count__gt=1
+        )
 
         objs.update(is_commercial=True)
 
