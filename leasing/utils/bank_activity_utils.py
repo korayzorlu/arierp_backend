9968090ec993c484e63ebc7a4929c424417f2157
korayzorlu <korayzorllu@gmail.com>
2025-10-06 12:09:39 +0300

    #251006 fix: fixed global settings

--- leasing/utils/bank_activity_utils.py
+++ leasing/utils/bank_activity_utils.py
@@ -1,5 +1,4 @@
-from leasing.models import BankActivity
-
 def match_bank_activity_from_iban(params):
+    from leasing.models import BankActivity
     objs = BankActivity.objects.select_related().filter(cross_bank_account_no = params.cross_bank_account_no).exclude(pk=params.exclude_pk)
     return objs
