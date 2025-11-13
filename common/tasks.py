16e2af078539678d4087568ecc2a956919e27ad9
korayzorlu <korayzorllu@gmail.com>
2025-11-13 07:57:58 +0300

    #251113 fix: fixed global settings

--- common/tasks.py
+++ common/tasks.py
@@ -58,11 +58,12 @@ def fetch_data_from_leaseflex(company):
 @shared_task()
 def fetch_big_data_from_leaseflex(company):
     fetch_contract_payments_from_leaseflex(company)
-    fetch_installments_from_leaseflex(company)
+    # fetch_installments_from_leaseflex(company)
 
 @shared_task()
 def fetch_very_big_data_from_leaseflex(company):
-    fetch_trade_transactions_from_leaseflex(company)
+    pass
+    # fetch_trade_transactions_from_leaseflex(company)
 
 @shared_task()
 def fetch_exchange_rates(target_currency,date):
