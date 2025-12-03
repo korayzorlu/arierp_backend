322273776e39ea50805492646238dc0f2e2db103
korayzorlu <korayzorllu@gmail.com>
2025-12-03 11:07:01 +0300

    #251203 fix: fixed global settings

--- leasing/api/filters.py
+++ leasing/api/filters.py
@@ -159,10 +159,16 @@ class InstallmentFilter(FilterSet):
         
 class BankActivityFilter(FilterSet):
     created_date = DateFromToRangeFilter(field_name = 'created_date')
+    third_person_status = CharFilter(method = 'filter_third_person_status')
     class Meta:
         model = BankActivity
         fields = ['uuid','bank','bank_account_no','process_type','receipt_no','description','created_date','third_person_status']
 
+    def filter_third_person_status(self, queryset, third_person_status, value):
+        if value == "all":
+            return queryset
+        else:
+            return queryset.filter(third_person_status=value)
 
 class BankActivityLeaseFilter(FilterSet):
     bank_activity = CharFilter(method = 'filter_bank_activity')
