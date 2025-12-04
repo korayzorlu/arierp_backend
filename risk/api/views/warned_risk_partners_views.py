0855d46e4550bf86ea53e5270802f971fb3ee888
korayzorlu <korayzorllu@gmail.com>
2025-12-04 17:14:11 +0300

    #251204 fix: fixed global settings

--- risk/api/views/warned_risk_partners_views.py
+++ risk/api/views/warned_risk_partners_views.py
@@ -177,7 +177,7 @@ class WarnedRiskPartnerList(ModelViewSet, QueryListAPIView):
             Q(partner_contracts__contract_leases__is_kdv_diff=False) &
             Q(partner_contracts__contract_leases__is_credit=False) &
             Q(partner_contracts__contract_leases__is_under_review=False) &
-            Q(partner_contracts__contract_warning_notices__official_cancellation_date__gt=datetime.today()) &
+            #Q(partner_contracts__contract_warning_notices__official_cancellation_date__gt=datetime.today()) &
             Q(partner_contracts__contract_leases__overdue_days__gt=30) &
             Q(partner_contracts__contract_leases__overdue_amount__gt=1000) &
             ~Q(partner_contracts__contract_leases__warning_notice_status='kapsamli_ihtar')
