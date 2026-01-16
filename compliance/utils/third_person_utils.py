9c15d4af628b41b74f3eae5e3e5ca1483780e0be
korayzorlu <korayzorllu@gmail.com>
2026-01-16 14:51:07 +0300

    #260116 fix: fixed global settings

--- compliance/utils/third_person_utils.py
+++ compliance/utils/third_person_utils.py
@@ -169,9 +169,14 @@ def check_third_person_in_partners():
         Q(status = 'need_document')
     )
     for obj in objs:
-        if Partner.objects.select_related().filter(Q(tc_vkn_no=obj.tc_vkn_no) | Q(vat_no=obj.tc_vkn_no)).exists():
+        if Partner.objects.select_related().filter(Q(tc_vkn_no=obj.tc_vkn_no) | Q(vat_no=obj.tc_vkn_no)).exists() and obj.tc_vkn_no and obj.tc_vkn_no != "":
             obj.status = 'cleared'
             obj.save()
+            bank_activities = obj.bank_activities.select_related().all()
+            bank_activities.update(
+                is_reliable_person = True,
+                third_person_status = 'cleared'
+            )
             send_email_for_third_person_to_cleared(obj.name,obj.tc_vkn_no)
 
         time.sleep(1)   
