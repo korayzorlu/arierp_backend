5bd1fc7724619c623a39f3b5ea7b6232732c2aea
korayzorlu <korayzorllu@gmail.com>
2026-02-03 11:01:15 +0300

    #260203 fix: fixed global settings

--- purchasing/admin.py
+++ purchasing/admin.py
@@ -7,16 +7,19 @@ from .models import PurchasePayment
 
 @admin.register(PurchasePayment)
 class PurchasePaymentAdmin(admin.ModelAdmin):
-    list_display = ["company"]
+    list_display = ["company", "lease"]
     list_display_links = []
-    search_fields = []
+    search_fields = ["lease__code", "company__name","lease__id"]
     list_filter = []
     inlines = []
     ordering = ["id"]
-    autocomplete_fields = ["company"]
+    autocomplete_fields = ["company", "lease"]
     
     def company(self,obj):
         return obj.company.name if obj.company else ""
+    
+    def lease(self,obj):
+        return obj.lease.code if obj.lease else ""
     
     class Meta:
         model = PurchasePayment
