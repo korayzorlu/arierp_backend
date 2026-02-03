6046ca981624139e376c6283b4d152823a95ec21
korayzorlu <korayzorllu@gmail.com>
2026-02-03 07:59:54 +0300

    #260203 fix: fixed global settings

---
+++ finance/management/commands/testleaseflexpaymentservice.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from contracts.models import *
+from finance.models import *
+from finance.tasks import fetch_partner_advances
+
+import pandas as pd
+import json
+import os
+import pyodbc
+
+from zeep import Client
+from zeep.transports import Transport
+import requests
+
+class Command(BaseCommand):
+    help = 'Exports parts to JSON file'
+    
+    def get_or_none(classmodel, **kwargs):
+        try:
+            return classmodel.objects.get(**kwargs)
+        except classmodel.DoesNotExist:
+            return None
+
+    def add_arguments(self, parser):
+        parser.add_argument('-c', type=str, help='Company to associate with operation')
+
+    def handle(self, *args, **options):
+        company = options.get('c')
+
+        print("processing...")
+
+        WSDL = "https://leaseflex.arileasing.com.tr/ARI_LEASING/Integration/Collection/VirtualPosIntegrationService.asmx?WSDL"
+
+        session = requests.Session()
+        transport = Transport(session=session, timeout=30)
+
+        client = Client(wsdl=WSDL, transport=transport)
+
+        result = client.service.Test()
+        print("TestResult:", result)
+
+        print("done!")
