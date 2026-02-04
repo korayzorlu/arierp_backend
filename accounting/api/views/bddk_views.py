b981c0a98913425c02be57a259c11bb7b03fabc0
korayzorlu <korayzorllu@gmail.com>
2026-02-04 13:57:37 +0300

    #260204 fix: fixed global settings

--- accounting/api/views/bddk_views.py
+++ accounting/api/views/bddk_views.py
@@ -443,11 +443,11 @@ class Kz222afList(ModelViewSet, QueryListAPIView):
                 #     yp_usd += objs.filter(Q(account_code__startswith=y) & Q(currency__code='USD')).aggregate(total_sum=Sum(F('total_debit_alternate') - F('total_credit_alternate')))['total_sum'] or Decimal('0.00')
                 #     yp_eur += objs.filter(Q(account_code__startswith=y) & Q(currency__code='EUR')).aggregate(total_sum=Sum(F('total_debit_alternate') - F('total_credit_alternate')))['total_sum'] or Decimal('0.00')
 
-                yp_query = Q()
-                for code in row_name['yps']:
-                    yp_query |= Q(account_code__startswith=code)
-
                 if row_name['yps']:
+                    yp_query = Q()
+                    for code in row_name['yps']:
+                        yp_query |= Q(account_code__startswith=code)
+                        
                     yp_usd_agg = objs.filter(yp_query, currency__code='USD').aggregate(
                         total_sum=Sum(F('total_debit_alternate') - F('total_credit_alternate')),
                         total_sum_tp=Sum(F('total_debit') - F('total_credit'))
