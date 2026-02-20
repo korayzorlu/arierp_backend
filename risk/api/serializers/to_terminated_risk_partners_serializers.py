95305c46474efdd7e35f4717904e71837e73a743
korayzorlu <korayzorllu@gmail.com>
2026-02-20 12:34:01 +0300

    #260220 fix: fixed global settings

--- risk/api/serializers/to_terminated_risk_partners_serializers.py
+++ risk/api/serializers/to_terminated_risk_partners_serializers.py
@@ -65,7 +65,7 @@ class ToTerminatedRiskPartnerListSerializer(serializers.Serializer):
             Q(is_kdv_diff=False) &
             Q(is_credit=False) &
             Q(is_under_review=False) &
-            #Q(contract__contract_warning_notices__official_cancellation_date__lte=datetime.today()) &
+            Q(contract__contract_warning_notices__official_cancellation_date__lte=datetime.today()) &
             Q(overdue_days__gt=25) &
             Q(overdue_amount__gt=1000)
         ).annotate(
