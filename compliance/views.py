c3c89b83737b88e16388ed5d797e7dff05c93b87
korayzorlu <korayzorllu@gmail.com>
2026-03-10 10:08:08 +0300

    #260310 fix: fixed global settings

--- compliance/views.py
+++ compliance/views.py
@@ -29,7 +29,7 @@ class UpdateThirdPersonStatusView(LoginRequiredMixin,View):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
 
-        if request.user.authorization.department != 'kredi_tahsis':
+        if request.user.authorization.department != 'kredi_tahsisss':
             return JsonResponse({'message': 'Bu işlem için yetkiniz yok!','status':'error'}, status=403)
 
         obj = ThirdPerson.objects.select_related().filter(uuid = data.get('uuid')).first()
