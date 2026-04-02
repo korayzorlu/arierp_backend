692d1a653ea4d7781064f3560671abdc9455d494
korayzorlu <korayzorllu@gmail.com>
2026-04-02 11:17:03 +0300

    #260402 fix: fixed global settings

--- contracts/sql/ihtarlar.sql
+++ contracts/sql/ihtarlar.sql
@@ -19,5 +19,5 @@ WHERE
     (PROCESS_SITUATION_ID is null or ResultId in (0,1,2)) 
     AND 1=1
     --AND CustomerId=29308
-    AND ResultId in (0,1)
+    AND ResultId in (0,1,2)
     -- AND ContractHeaderId = '57797'
