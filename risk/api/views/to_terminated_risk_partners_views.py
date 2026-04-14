c068e119f2cb29a5b3f8717370a05e73793de83e
korayzorlu <korayzorllu@gmail.com>
2026-04-14 09:04:43 +0300

    #260414 fix: fixed global settings

--- risk/api/views/to_terminated_risk_partners_views.py
+++ risk/api/views/to_terminated_risk_partners_views.py
@@ -161,7 +161,7 @@ class ToTerminatedRiskPartnerList(ModelViewSet, QueryListAPIView):
             Q(partner_contracts__contract_leases__is_kdv_diff=False) &
             Q(partner_contracts__contract_leases__is_credit=False) &
             Q(partner_contracts__contract_leases__is_under_review=False) &
-            Q(partner_contracts__contract_warning_notices__service_Date__isnull=False) &
+            Q(partner_contracts__contract_warning_notices__service_date__isnull=False) &
             Q(partner_contracts__contract_comprehensive_warning_notices__official_cancellation_date__lte=datetime.today()) &
             Q(partner_contracts__contract_leases__overdue_days__gt=25) &
             Q(partner_contracts__contract_leases__overdue_amount__gt=1000)
