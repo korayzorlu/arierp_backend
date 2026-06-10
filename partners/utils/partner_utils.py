178fbf8491a6aecc1bf3488f4661453004c7af96
korayzorlu <korayzorllu@gmail.com>
2026-06-10 14:06:11 +0300

    #260610 fix: fixed global settings

--- partners/utils/partner_utils.py
+++ partners/utils/partner_utils.py
@@ -526,7 +526,15 @@ def check_partner_ids(company):
     id_list = df['ID'].tolist()
 
     objs_count = Partner.objects.select_related().filter().count()
-    objs = Partner.objects.select_related().filter(crm_code__in=id_list)
+
+    duplicate_crm_codes = (
+        Partner.objects.filter(crm_code__in=id_list)
+        .values('crm_code')
+        .annotate(count=Count('id'))
+        .filter(count__gt=1)
+        .values_list('crm_code', flat=True)
+    )
+    objs = Partner.objects.select_related().filter(crm_code__in=duplicate_crm_codes)
 
     print(f"{objs_count} - {objs.count()}")
     print("--------")
