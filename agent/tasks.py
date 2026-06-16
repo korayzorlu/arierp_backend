ef5653750b9ac864a0830cb9d5e3d7b3536bfc84
korayzorlu <korayzorllu@gmail.com>
2026-06-16 14:44:26 +0300

    #260616 fix: fixed global settings

--- agent/tasks.py
+++ agent/tasks.py
@@ -3,6 +3,7 @@ from core.celery import app
 from django.http import JsonResponse
 
 from agent.utils.common_utils import AgentEngine
+from common.utils.websocket_utils import send_alert,send_agent_status
 
 @shared_task(bind=True)
 def agentData(self,df_json,user_id,agent_name,lf_username, lf_password):
@@ -13,3 +14,6 @@ def agentData(self,df_json,user_id,agent_name,lf_username, lf_password):
 def reject_pending_agent_task(agent_task_id):
     from agent.models import AgentTask
     AgentTask.objects.filter(id=agent_task_id, status='pending').update(status='rejected')
+    if AgentTask.objects.filter(id=agent_task_id, status='pending').exists():
+        send_agent_status({"running": False}, room=f"private_{agent_task_id}")
+        send_alert({"message": "Agent görevi reddedildi!", 'status': 'error'}, room=f"private_{agent_task_id}")
