a304e1c0fe23a00e0a8de2793e4e872d79f65b18
korayzorlu <korayzorllu@gmail.com>
2026-06-30 09:55:23 +0300

    #260630 fix: fixed global settings

--- trade/api/serializers.py
+++ trade/api/serializers.py
@@ -85,7 +85,7 @@ class TradeTransactionListSerializer(serializers.Serializer):
         return obj.currency.code if obj.currency else ''
     
     def get_due_date(self, obj):
-        return localtime(obj.due_date.date()) if obj.due_date else ''
+        return localtime(obj.due_date).date() if obj.due_date else ''
 
     def get_balances(self, obj):
         objs = TradeTransaction.objects.select_related().filter(lease = obj.lease).exclude(delete_status__in=['2']).order_by('posting_group_id','due_date','record_date','trade_transaction_id')
